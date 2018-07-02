73f25e2adbf656c6d8610e03f4283c72f163e940
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-07-02 14:43:04 +0100

    update about page

--- src/About.vue
+++ src/About.vue
@@ -3,7 +3,7 @@
       <h1 id='title' class="title is-2">About SiSEC MUS 2018</h1>
 
     <div class="content">
-      <h1 class='title'>Authors of this website</h1>
+      <h1 class='title'>SiSEC 2018 MUS Organizers</h1>
       <article class="media">
       <div class="media-content">
         <div class="content">
@@ -29,45 +29,65 @@
   </div>
   </article>
     </div>
-    <!-- <div class="content">
-      <h1 class='title'>SiSEC Software</h1>
+    <div class="content">
+      <h1 class='title'>SiSEC Repository Eco-System</h1>
       <ul>
-        <li><a href="https://github.com/faroit/sisec-mus-website">
+        <li><a href="https://github.com/sigsep/sigsep-mus-2018">
           <span><i class="fa fa-github"></i></span>
-          <span>Source Code for this Website</span>
+          <span>2018 Objective Scores Submissions</span>
           </a>
         </li>
-        <li><a href="https://github.com/faroit/sisec-mus-results">
+        <li><a href="https://github.com/sigsep/sigsep-mus-2018-analysis">
           <span><i class="fa fa-github"></i></span>
-          <span>Results and Statistical Evaluation</span>
+          <span>2018 Objective Scores Stastical Evaluation</span>
           </a>
         </li>
-        <li><a href="https://github.com/faroit/dsdtools">
+        <li><a href="https://zenodo.org/communities/sigsep/?page=1&size=20">
+          <span>2018 Audio Results</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/sigsep/sigsep-mus-db">
           <span><i class="fa fa-github"></i></span>
-          <span>Python parser for the DSD100 dataset</span>
+          <span>Python parser for the MUSDB18 dataset</span>
           </a>
         </li>
 
-        <li><a href="https://github.com/faroit/sisec-mus-previews">
+        <li><a href="https://github.com/sigsep/sigsep-mus-cutlist-generator">
           <span><i class="fa fa-github"></i></span>
-          <span>Select excerpts for 30s preview</span>
+          <span>Code to select 30s previews</span>
           </a>
         </li>
-        <li><a href="https://github.com/faroit/sisec-mus-trim">
+        <li><a href="https://github.com/sigsep/sigsep-mus-preview-generator">
           <span><i class="fa fa-github"></i></span>
           <span>Trim estimates based on 30s preview selection</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/sigsep/pub-SiSEC2018">
+          <span><i class="fa fa-github"></i></span>
+          <span>SiSEC Paper LaTeX Code</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/sigsep/sigsep-mus-2018-slides">
+          <span><i class="fa fa-github"></i></span>
+          <span>SiSEC 2018 Presentation Slides</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/sigsep/sigsep-mus-2018-website">
+          <span><i class="fa fa-github"></i></span>
+          <span>Source Code for this Website</span>
           </a>
         </li>
       </ul>
     </div>
 
     <div class="content">
-      <h1 class='title'>Additional Software</h1>
+      <h1 class='title'>Open Source libs used in this website</h1>
       <ul>
-        <li><a href="https://vuejs.org/">vue.js</a></li>
-        <li><a href="https://github.com/naomiaro/waveform-playlist">waveform-playlist</a></li>
+        <li><a href="https://vuejs.org/">vue.js: Javascript Framework to power this website </a></li>
+        <li><a href="https://github.com/naomiaro/waveform-playlist">waveform-playlist: Multitrack player used here</a></li>
+        <li><a href="https://github.com/bulma/bulma">bulma: CSS Framework</a></li>
       </ul>
-    </div> -->
+    </div>
 
   </section>
 </template>
